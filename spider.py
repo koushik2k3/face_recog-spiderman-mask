cfe5cb54b6b78214642006ea412264712668c743
Koushik KP <koushikkp2003@gmail.com>
2024-04-08 20:10:00 +0530

    Update spider.py

--- spider.py
+++ spider.py
@@ -25,7 +25,6 @@ while(True):
     cv2.imshow('frame', frame)
        
     # the 'q' button is set as the
-    # quitting button you may use any
     # desired button of your choice
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
